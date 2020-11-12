022e7572ea35faf42459bf210298af6d3aff2e7f
Gilbert Yap <gilbert.yap16@gmail.com>
2020-11-12 11:22:29 -0500

    Update download process to have video and audio conversion

--- Sprint4/downloadVideos.py
+++ Sprint4/downloadVideos.py
@@ -1,6 +1,7 @@
 # Download YouTube videos from a text file of youtube links
-
+# Then separate out the audio file and video file
 from pytube import YouTube
+from moviepy.editor import *
 import os, random
 
 print('Getting all VoxCeleb1 links...')
@@ -13,7 +14,7 @@ downloadPath = './downloaded_videos/'
 if not os.path.isdir(downloadPath):
     os.mkdir(downloadPath)
 
-numVideos = 10
+numVideos = 2
 print('Downloading {} random links...'.format(numVideos))
 i = 0
 history = []
@@ -38,3 +39,31 @@ while i < numVideos:
             continue
 
 print('Finished downloading all videos')
+
+# Make conversion folder
+convertFolder = './convertedFiles/'
+audioFolder = convertFolder+'audio/'
+videoFolder = convertFolder+'video/'
+if not os.path.isdir(convertFolder):
+    os.mkdir(convertFolder)
+if not os.path.isdir(audioFolder):
+    os.mkdir(audioFolder)
+if not os.path.isdir(videoFolder):
+    os.mkdir(videoFolder)
+
+videoList = os.listdir(downloadPath)
+for video in videoList:
+    (fileName, fileExt) = os.path.splitext(video)
+    try:
+        convertVideo = VideoFileClip(downloadPath+video)
+        convertAudio = convertVideo.audio
+        convertAudio = convertAudio.fx(afx.audio_normalize)
+        convertAudio.write_audiofile(audioFolder+fileName+'.wav', fps=48000, nbytes=2, codec='pcm_s16le')
+        convertVideo.write_videofile(videoFolder+fileName+'.mp4',fps=25)
+        convertVideo.close()
+        convertVideo.close()
+    except:
+        print('Could not convert {}'.format(video))
+        continue
+
+print('Finished converting all videos.')
