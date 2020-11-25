ffe9905d0f7d489901f0a750c4d3a0719a595114
Gilbert Yap <gilbert.yap16@gmail.com>
2020-11-25 10:58:08 -0500

    Update diarization script, adds script for manually identifying audio

---
+++ Sprint5/generateScoreRttms.py
@@ -0,0 +1,20 @@
+# load pipeline
+import torch
+import os
+
+audioFileList = os.listdir('./convertedFiles/audio/')
+scoresFileList = os.listdir('./convertedFiles/scoreRttms/')
+
+pipeline = torch.hub.load('pyannote/pyannote-audio', 'dia')
+
+for file in audioFileList:
+    fileName, fileExt = os.path.splitext(file)
+    scoreFileName = fileName+'_score.rttm'
+    if not (scoreFileName in scoresFileList):
+        print('Diarizing {}'.format(file))
+        # apply diarization pipeline on your audio file
+        diarization = pipeline({'audio': './convertedFiles/audio/'+file})
+
+        # dump result to disk using RTTM format
+        with open(scoreFileName, 'w') as f:
+            diarization.write_rttm(f)
