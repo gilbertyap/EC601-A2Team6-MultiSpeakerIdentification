ff8511a293e8f63106c9b7c1f0fc52520ff530f1
Gilbert Yap <gilbert.yap16@gmail.com>
2020-12-08 00:06:12 -0500

    Add batch scripts for Windows and Linux

---
+++ getVsdRttms.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+python3 findMouthThresholdValue.py -v ./convertedFiles/video/_Qupc7kvV5o.mp4
+python3 findMouthThresholdValue.py -v ./convertedFiles/video/7AJtb-uSTr4.mp4
+python3 findMouthThresholdValue.py -v ./convertedFiles/video/ja2jQgAysqw.mp4
+python3 findMouthThresholdValue.py -v ./convertedFiles/video/nGrB-5ieeMU.mp4
+python3 findMouthThresholdValue.py -v ./convertedFiles/video/riB39LVbAG0.mp4
+python3 findMouthThresholdValue.py -v ./convertedFiles/video/zWluaKRyeQw.mp4
